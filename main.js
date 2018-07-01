4894dce036f7eb42e535674e322964198e7ddd11
Stephen Olshefski <stephen.olshefski@outlook.com>
2018-06-30 20:06:16 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//Sidenav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+//Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+});
+//Auto complete
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+    data: {
+        "Aruba": null,
+        "Cancun": null,
+        "Hawaii": null,
+        "Florida": null,
+        "California": null,
+        "Jamaica": null,
+        "Europe": null
+    }
+});
+//Material Boxed
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {});
+//Scrollspy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
